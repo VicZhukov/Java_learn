35ef4c6dc2f1706a52ba585072fae594076fe40b
Victor <32028809+VicZhukov@users.noreply.github.com>
2022-10-15 20:26:39 +0300

    Exercise 5 - done!

--- src/Module_5/SpaceShip.java
+++ src/Module_5/SpaceShip.java
@@ -22,9 +22,13 @@ public class SpaceShip {
     }
     public void setSerialNumber(String serialNumber){
         if(serialNumber.charAt(0) == 'S' && serialNumber.charAt(1) == 'N' && serialNumber.length() == 8){
-            this.serialNumber = serialNumber;;
+            this.serialNumber = serialNumber;
         }
 
+    }
+
+    public void printInfo(){
+        System.out.println("Name is " + getName() + "," + " " + "serial number is " + getSerialNumber());
     }
 
     public static void main(String[] args) {
@@ -38,12 +42,18 @@ public class SpaceShip {
             System.out.println(ship.getName()); //Should be Walker, empty value ignored
 
             ship.setName("Voyager ".repeat(100));
-            System.out.println(ship.getName()); //Should be Walker, too long value ignored */
+            System.out.println(ship.getName()); //Should be Walker, too long value ignored
 
             ship.setSerialNumber("SN506788");
             System.out.println(ship.getSerialNumber()); //Should be SN506788
 
             ship.setSerialNumber("EE123456");
-            System.out.println(ship.getSerialNumber()); //Should be SN506788 - old value
+            System.out.println(ship.getSerialNumber()); //Should be SN506788 - old value*/
+
+            ship.setName("Voyager");
+            ship.setSerialNumber("SN506788");
+
+            //Name is Voyager, serial number is SN504030
+            ship.printInfo();
     }
 }
