6c8c35e0ee6633a12abd3a7156b07f85bab09fde
Victor <32028809+VicZhukov@users.noreply.github.com>
2022-10-17 15:34:11 +0300

    overriding methods equals() and hashCode() ver 2.0

--- src/Module_5/Point.java
+++ src/Module_5/Point.java
@@ -67,7 +67,65 @@ class PointTest {
         p3.setY(2);
         p3.setZ(3);
 
-        System.out.println(p1.equals(p2)); //Should be true
-        System.out.println(p1.equals(p3)); //Should be false
+        /*System.out.println(p1.equals(p2)); //Should be true
+        System.out.println(p1.equals(p3)); //Should be false*/
+
+        StarTrack track1 = new StarTrack();
+        track1.setStart(p1);
+        track1.setFinish(p2);
+
+        StarTrack track2 = new StarTrack();
+        track2.setStart(p1);
+        track2.setFinish(p2);
+
+        StarTrack track3 = new StarTrack();
+        track3.setStart(p1);
+        track3.setFinish(p3);
+
+        System.out.println(track1.equals(track2)); //Should be true
+        System.out.println(track1.equals(track3)); //Should be false
+        System.out.println(track1.hashCode() == track2.hashCode()); //Should be true
+    }
+}
+class StarTrack{
+    private Point start;
+    private Point finish;
+
+    public void setStart(Point start){
+        this.start = start;
+    }
+    public Point getStart(){
+        return start;
+    }
+
+    public void setFinish(Point finish){
+        this.finish = finish;
+    }
+    public Point getFinish(){
+        return finish;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        StarTrack point = (StarTrack) obj;
+        return (start == point.start || (start != null && start.equals(point.getStart())))
+                && (finish == point.finish || (finish != null && finish.equals(point.getFinish())));
+    }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((start == null) ? 0 : start.hashCode());
+        result = prime * result + ((finish == null) ? 0 : finish.hashCode());
+
+
+        return result;
     }
 }
