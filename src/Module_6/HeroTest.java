cdecd9f7b26d284b63c5399d0d423aa018bb5346
Victor <32028809+VicZhukov@users.noreply.github.com>
2022-10-20 08:29:03 +0300

    Module_6 Trying constructor for the first time ex 1

---
+++ src/Module_6/HeroTest.java
@@ -0,0 +1,30 @@
+package Module_6;
+
+class HeroTest{
+    public static void main(String[] args) {
+        Hero hero = new Hero();
+
+        //Expect Paratrooper
+        System.out.println(hero.getName());
+
+        //Expect 100
+        System.out.println(hero.getHp());
+    }
+}
+class Hero{
+    private String name;
+    private int hp;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public Hero(){
+        this.name = "Paratrooper";
+        this.hp = 100;
+    }
+}
