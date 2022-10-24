de9f128d9715966ada7c3d1f2390ee94aeb99428
Victor <32028809+VicZhukov@users.noreply.github.com>
2022-10-24 21:53:14 +0300

    Module_6 ex 18 nested class LevelInfo + reoverrided toString

--- src/Module_6/LevelTest.java
+++ src/Module_6/LevelTest.java
@@ -4,8 +4,9 @@ import java.util.Arrays;
 
 public class LevelTest {
     public static void main(String[] args) {
+        // ex 16
         //Quarke level, name is Test
-        //System.out.println(new Level("Test")); // ex 16
+        //System.out.println(new Level("Test"));
 
         // ex 17
         Level.Point p1 = new Level.Point(3, 5);
@@ -15,19 +16,25 @@ public class LevelTest {
         Level.Point[] points = {p1, p2, p3};
 
         //Quarke level, name is Test, point count is 3
-        System.out.println(new Level("Test", points));
+        //System.out.println(new Level("Test", points));
+
+        // ex 18
+        Level.LevelInfo info = new Level.LevelInfo("Quarke Intro", "Easy");
+
+        //Quarke level, name is Quarke Intro, difficulty is Easy, point count is 3
+        System.out.println(new Level(info, points));
     }
 }
 class Level{
-    private String name;
+    private LevelInfo levelInfo;
     private Point[] points;
 
-    public Level(String name){
+    /*public Level(String name){
         this.name = name;
-    }
+    }*/
 
-    public Level(String name, Point[] points) {
-        this.name = name;
+    public Level(LevelInfo levelInfo, Point[] points) {
+        this.levelInfo = levelInfo;
         this.points = points;
     }
 
@@ -36,14 +43,12 @@ class Level{
         //return "Quarke level, name is " + name;
         int count = 0;
         String[] res = Arrays.toString(points).split("\\s+");
-
         for (int i = 1; i <= res.length; i++) {
             count++;
         }
-        return "Quarke level, name is " + name + ", point count is " + count;
+        return "Quarke level, name is " + levelInfo.name + ", difficulty is " + levelInfo.difficulty + ", point count is " + count;
 
     }
-
     static class Point{
         private int x;
         private int y;
@@ -57,6 +62,23 @@ class Level{
         public Point(int x, int y) {
             this.x = x;
             this.y = y;
+        }
+    }
+    static class LevelInfo{
+        private String name;
+        private String difficulty;
+
+        public String getName() {
+            return name;
+        }
+
+        public String getDifficulty() {
+            return difficulty;
+        }
+
+        public LevelInfo(String name, String difficulty) {
+            this.name = name;
+            this.difficulty = difficulty;
         }
     }
 }
