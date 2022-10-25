5fe8a266cb8e467a0019b34baeabc3c4a8d6eeaf
Victor <32028809+VicZhukov@users.noreply.github.com>
2022-10-25 21:47:30 +0300

    Module_6 ex 22 creating Singleton class "LevelLoader"

---
+++ src/Module_6/LevelLoaderTest.java
@@ -0,0 +1,26 @@
+package Module_6;
+
+class LevelLoader{
+    private static LevelLoader instance = new LevelLoader();
+
+    public static LevelLoader getInstance(){
+        return instance;
+    }
+    String load(String levelName){
+        return "Loading level " + levelName + " ...";
+    }
+}
+
+public class LevelLoaderTest {
+    public static void main(String[] args) {
+        LevelLoader levelLoader = new LevelLoader();
+        //ex 21
+        //Loading level Startlevel ...
+        //System.out.println(levelLoader.load("StartLevel"));
+        // ex 22
+        //Loading level Startlevel ...
+        System.out.println(LevelLoader.getInstance().load("StartLevel"));
+        System.out.println(LevelLoader.getInstance() == LevelLoader.getInstance());
+    }
+}
+
