f9356040a03b891d87fc6f9474097bca4ea661f5
Victor <32028809+VicZhukov@users.noreply.github.com>
2022-10-30 22:05:14 +0200

    Module_7 ex 26 appending string array using stringBuilder

---
+++ src/Module_7/PhraseMaker.java
@@ -0,0 +1,24 @@
+package Module_7;
+
+import java.util.Arrays;
+
+public class PhraseMaker {
+    public String join(String[] words) {
+        StringBuilder result = new StringBuilder();
+        for(String i : words){
+            if(i.length() > 3){
+                i = i.toLowerCase();
+            }else i = i.toUpperCase();
+            result.append(i).append(" ");
+        }
+        return result.toString().strip();
+    }
+}
+class PhraseMakerTest {
+    public static void main(String[] args) {
+        String[] words = {"Life", "is", "great", "thing"};
+
+        //life IS great thing
+        System.out.println(new PhraseMaker().join(words));
+    }
+}
