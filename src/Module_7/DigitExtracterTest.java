17c5c43101e48821861409f98cbbf0ed88446da6
Victor <32028809+VicZhukov@users.noreply.github.com>
2022-10-30 20:44:16 +0200

    Module_7 ex 19 extracting numbers from string to int array

---
+++ src/Module_7/DigitExtracterTest.java
@@ -0,0 +1,25 @@
+package Module_7;
+
+import java.util.Arrays;
+
+public class DigitExtracterTest {
+    public static void main(String[] args) {
+        DigitExtracter digitExtracter = new DigitExtracter();
+
+        int[] extracted = digitExtracter.extract("april 5, year 2000");
+
+        //[5, 2, 0, 0, 0]
+        System.out.println(Arrays.toString(extracted));
+    }
+}
+class DigitExtracter{
+    public int[] extract(String text){
+        String array = text.replaceAll("\\D+", "");
+
+        int[] result = new int[array.length()];
+        for (int i = 0; i < array.length(); i++) {
+            result[i] = Integer.parseInt(String.valueOf(array.charAt(i)));
+        }
+        return result;
+    }
+}
