2b3c701b3eae54abe936e500dc34040c49a07e1c
Victor <32028809+VicZhukov@users.noreply.github.com>
2022-10-30 21:49:38 +0200

    Module_7 ex 25 striping numbers using stringBuilder

---
+++ src/Module_7/NumberJoiner.java
@@ -0,0 +1,17 @@
+package Module_7;
+
+public class NumberJoiner {
+    public String join(int[] numbers) {
+        StringBuilder result = new StringBuilder();
+        for(int number: numbers) {
+            result.append(number);
+        }
+        return result.toString().strip();
+    }
+}
+class NumberJoinerTest {
+    public static void main(String[] args) {
+        //102030
+        System.out.println(new NumberJoiner().join(new int[] {10, 20, 30}));
+    }
+}
