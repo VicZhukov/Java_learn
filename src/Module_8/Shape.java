fbefb134dca79c9f0d4a0562fe7730f5815ee196
Victor <32028809+VicZhukov@users.noreply.github.com>
2022-10-31 12:56:27 +0200

    Module_8 First homework rebuilding code with interface

--- src/Module_8/Shape.java
+++ src/Module_8/Shape.java
@@ -1,58 +1,51 @@
 package Module_8;
 
-class Shape {
-    private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Shape(String name) {
-        this.name = name;
+public interface Shape {
+    void name();
+}
+class Circle implements Shape{
+    @Override
+    public void name(){
+        System.out.println("Circle");
     }
 }
-class Circle extends Shape{
-    public Circle(String name) {
-        super(name);
+class Quad implements Shape{
+    @Override
+    public void name() {
+        System.out.println("Quad");
     }
 }
-class Quad extends Shape{
-    public Quad(String name) {
-        super(name);
+class Triangle implements Shape{
+    @Override
+    public void name() {
+        System.out.println("Triangle");
     }
 }
-class Triangle extends Shape{
-    public Triangle(String name) {
-        super(name);
+class Squad implements Shape{
+    @Override
+    public void name() {
+        System.out.println("Squad");
     }
 }
-class Squad extends Shape{
-    public Squad(String name) {
-        super(name);
-    }
-}
-class Oval extends Shape{
-    public Oval(String name) {
-        super(name);
+class Oval implements Shape{
+    @Override
+    public void name() {
+        System.out.println("Oval");
     }
 }
 class ShapeTest{
     public static void main(String[] args) {
-        Shape circle = new Circle("circle");
-        circle.setName("Circle");
-        System.out.println(circle.getName());
-        Shape quad = new Quad("Quad");
-        System.out.println(quad.getName());
-        Shape triangle = new Triangle("Triangle");
-        System.out.println(triangle.getName());
-        Shape squad = new Squad("Squad");
-        System.out.println(squad.getName());
-        Shape oval = new Oval("Oval");
-        System.out.println(oval.getName());
+        Shape circle = new Circle();
+        Shape quad = new Quad();
+        Shape squad = new Squad();
+        Shape triangle = new Triangle();
+        Shape oval = new Oval();
+
+        circle.name();
+        quad.name();
+        squad.name();
+        triangle.name();
+        oval.name();
     }
 }
 
