60dd692e0a6ff20683ca9dc67f9bfd67d10fe6af
Victor <32028809+VicZhukov@users.noreply.github.com>
2022-11-18 09:58:52 +0200

    Module_11 Task2 sorting List by second element of string in revers (Z-A)

--- src/module_11/Task2.java
+++ src/module_11/Task2.java
@@ -1,7 +1,20 @@
 package module_11;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Task2 {
     public static void main(String[] args) {
+        Stream<String> names = Stream.of("1. Ivan", "2. Vic", "3. Peter", "4. Max", "5. Alex")
 
+                .sorted(Collections.reverseOrder())
+                .sorted(Comparator.comparing(str -> str.split(" ")[1]));
+
+                List<String> result = names.collect(Collectors.toList());
+
+        System.out.println(result);
     }
 }
