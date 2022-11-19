55f53b7f56161663fdab00d86b82d2817b68cf89
Victor <32028809+VicZhukov@users.noreply.github.com>
2022-11-19 13:49:22 +0200

    Module_11 Task3 array of strings parsing to int and sort

--- src/module_11/Task3.java
+++ src/module_11/Task3.java
@@ -1,9 +1,18 @@
 package module_11;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Task3 {
     private static String[] numbers = {"1, 2, 0", "4, 5"};
-
     public static void main(String[] args) {
-
+        String values = String.join(", ", numbers);
+        String[] nums = values.split(", ");
+        List<Integer> result = Arrays.stream(nums)
+                .map(Integer::parseInt)
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println(result);
     }
 }
