4df01c984b1a8fbbb857308e7387b54bc63b1513
Victor <32028809+VicZhukov@users.noreply.github.com>
2022-11-21 11:17:50 +0200

    Module_11 Task4 creating linear congruent method

--- src/module_11/Task4Stream.java
+++ src/module_11/Task4Stream.java
@@ -7,7 +7,7 @@ public class Task4Stream {
     public static void main(String[] args) {
         Task4 rand = new Task4(25214903917l,2^48);
         Stream<Long> stream =
-                Stream.iterate(1l, (seed) -> rand.c(seed).next()
+                Stream.iterate(11l, (seed) -> rand.c(seed).next()
                 );
         stream
                 .limit(10)
